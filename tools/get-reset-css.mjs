98abe90b1ab8f47cc4114ed84ba4cdf683eeabc7
mstssk <mstssk+github@gmail.com>
2025-03-15 07:50:44 +0900

    unpkg.comの状態に引き摺られないためにreset.cssを自前で持っておく

---
+++ tools/get-reset-css.mjs
@@ -0,0 +1,21 @@
+import { writeFileSync } from "node:fs";
+
+const tag = await getLatestReleaseTag();
+const css = await fetchCSS(tag);
+writeFileSync("reset.css", css);
+
+async function getLatestReleaseTag() {
+  const url = "https://api.github.com/repos/mayank99/reset.css/releases";
+  const res = await fetch(url);
+  const data = await res.json();
+  /** @type string */
+  const tag = data[0].tag_name;
+  return tag;
+}
+
+async function fetchCSS(tag) {
+  const url = `https://raw.githubusercontent.com/mayank99/reset.css/refs/tags/${tag}/package/index.css`;
+  const res = await fetch(url);
+  const file = await res.text();
+  return `/* Use https://github.com/mayank99/reset.css ${tag} */\n\n${file}`;
+}
